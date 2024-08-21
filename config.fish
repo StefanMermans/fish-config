2a5903bb072b6634824a07e42362e335fe6ad921
Stefan Mermans <stefanmermans99@gmail.com>
2024-08-21 16:16:18 +0200

    Cleanup fish config

--- config.fish
+++ config.fish
@@ -1,12 +1,24 @@
 set -x NVM_DIR "$HOME/.nvm"
-set fish_greeting ""
+set -g fish_greeting ""
 
 load_nvm > /dev/stderr
 
 # Commands to run in interactive sessions can go here
 if status is-interactive
-    eval (/opt/homebrew/bin/brew shellenv)
+    set -l normal_color (set_color normal)
+
+    # Idk what this does really
     set -x GPG_TTY (tty)
-    nvm use node
+    eval (/opt/homebrew/bin/brew shellenv)
+
+    # Node
+    set -l node_color (set_color 77dd77)
+    nvm use node --silent
+    echo -s $node_color "Node: " $normal_color (node --version)
+    
+    # PHP
+    set -l php_color (set_color ee7777)
+    set -l php_version (php --version | head -n 1 | grep -oE "\d+\.\d+\.\d+")
+    echo -s $php_color "PHP:  " $normal_color "v" $php_version
 end
 
